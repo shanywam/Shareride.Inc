5e1aae20ed211dfb2ec95b397bab1c43415a9bed
shannelle <shannellenjeru@gmail.com>
2019-04-05 18:44:34 +0300

    updating work done

---
+++ client/ride.php
@@ -0,0 +1,145 @@
+<?php
+//shannelle
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href ="../css/bootstrap.min.css"/>
+    <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src ="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Shareride.Inc</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="main-header-nav" class="collapse navbar-collapse">
+        <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
+            <li class="nav-item active">
+                <a class="nav-link" href="../index.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="../driver/drive.php"><i style ="color:darkgrey ;"class="fa fa-car"></i> Drive <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="../client/ride.php"><i style ="color:darkgrey ;"class="fa fa-user"></i> Ride <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+
+    <div id="ra_header_container_5ca64c7c3a2e8" class="modules-container ra_header_container_5ca64c7c3a2e8  vc_custom_1528890373974"></div>
+    <script type="text/javascript">(function($) {$("head").append("<style>.rella-row-shadowbox-5ca64c7c34b20{-webkit-box-shadow:;-moz-box-shadow:;box-shadow:;}</style>");})(jQuery);</script>
+
+
+
+    <form class="form-inline my-2 my-lg-0">
+        <button type="button" class="btn btn-light" <i style ="color:darkgrey; "class="fa fa-user"></i> Ride with Shareride </button>
+        <button type="button" class="btn btn-primary"><i style ="color:darkgrey; "class="fa fa-car"></i> Register Driver</button>
+    </form>
+
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../image/driver.jpg" class="d-block w-100" alt="..." style="height:400px;width:50%;color:rgba(255,255,255,1);border-radius:20px ">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col"><br><br>
+            <h1><span style="color: black;">Ride with Shareride.Inc</span></h1>
+            <p>Use Shareride.Inc to get rides in minutes</p>
+            <p>Pick the ride you need, then a registered driver will be their to take you to wherever you want. </p>
+            <p>The rides are smooth as they can be with minimal interference.</p>
+            <p>Try today and be the first to experience what we have to offer</p>
+            <p>Riding with Shareride.Inc is different because we are locally-based in Nairobi. We treat our client with recpect and car&#8217;e .</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+
+        <div class="col"><br><br>
+            <h1><span style="color: black;">Our Drivers</span></h1>
+            <p>Our drives are very professional in what they do</p>
+            <p>They are licenced and registered to to drive clients to where they wan to go<p>
+            <p>They are also very trustworthy </p>
+        </div>
+        <div class="col">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../image/taxi.jpg" class="d-block w-100" alt="..." style="height:400px;width:50%;color:rgba(255,255,255,1);border-radius:20px ">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div>
+        <div style="background-color:rgba(211,211,211,0.5);">
+            <br/><br/><br/>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <b><h>About</h></b><br/><br/>
+                        <h style="color:grey">Shareride.Inc is a ride hailing website based in Nairobi, Kenya.</h><br/>
+                        <h style="color:grey">we provide safe and reliable transport in the city of Nairobi.</h><br/>
+                        <br/><br/>
+
+                    </div>
+                    <div class="col-sm">
+                        <b><h>Services</h></b><br/><br/>
+                        <h style="color:grey">Register to be a driver</h><br/>
+                        <h style="color:grey">Book for a ride</h><br/>
+
+                    </div>
+                    <div class="col-sm">
+                        <b><h>Contacts</h></b><br/><br/>
+                        <h style="color:grey">+254 71 958 2000</h><br/>
+                        <h style="color:grey">+254 70 765 8859 </h><br/>
+                        <h style="color:grey">+254 72 086 3269</h><br/>
+                        <h style="color:grey">+254 71 442 8358 </h><br/>
+                        <br/><br/>
+                    </div>
+                    <div class="col-sm">
+                        <b><h>Emails</h></b><br/><br/>
+                        <h style="color:grey">pnjeru_@shareride.com</h>
+                        <h style="color:grey">nshawn@shareride.com</h>
+                        <h style="color:grey">lnnderitu@shareride.com</h>
+                        <br/><br/>
+
+                        <br/><br/><br/><br/><br/>
+                        <p style="float:right"><font size="-1">2019 Shareride.Inc</font></p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                    </div>
+                    <div class="col-sm">
+                        <center><p><font size="-1">Privacy  Terms  Legal  Site Map  Site Feedback</font></p></center>
+                    </div>
+                    <div class="col-sm">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</body>
+>
+</html>
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <link rel="stylesheet" href ="./css/bootstrap.min.css"/>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src ="./js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
@@ -18,26 +18,17 @@
 
     <div id="main-header-nav" class="collapse navbar-collapse">
         <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
-            <li class="nav-item">
-                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-car"></i> Drive <span class="sr-only">(current)</span></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="driver/drive.php"><i style ="color:darkgrey ;"class="fa fa-car"></i> Drive <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-user"></i> Ride <span class="sr-only">(current)</span></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="client/ride.php"><i style ="color:darkgrey ;"class="fa fa-user"></i> Ride <span class="sr-only">(current)</span></a>
             </li>
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Menu
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Service</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Others</a>
-                </div>
-            </li>
+
         </ul>
 
     </div>
@@ -62,6 +53,58 @@
 
     </div>
 </div>
+<div>
+
+    <div style="background-color:rgba(211,211,211,0.5);">
+        <br/><br/><br/>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <b><h>About</h></b><br/><br/>
+                    <h style="color:grey">Shareride.Inc is a ride hailing website based in Nairobi, Kenya.</h><br/>
+                    <h style="color:grey">we provide safe and reliable transport in the city of Nairobi.</h><br/>
+                    <br/><br/>
+
+                </div>
+                <div class="col-sm">
+                    <b><h>Services</h></b><br/><br/>
+                    <h style="color:grey">Register to be a driver</h><br/>
+                    <h style="color:grey">Book for a ride</h><br/>
+
+                </div>
+                <div class="col-sm">
+                    <b><h>Contacts</h></b><br/><br/>
+                    <h style="color:grey">+254 71 958 2000</h><br/>
+                    <h style="color:grey">+254 70 765 8859 </h><br/>
+                    <h style="color:grey">+254 72 086 3269</h><br/>
+                    <h style="color:grey">+254 71 442 8358 </h><br/>
+                    <br/><br/>
+                </div>
+                <div class="col-sm">
+                    <b><h>Emails</h></b><br/><br/>
+                    <h style="color:grey">pnjeru_@shareride.com</h>
+                    <h style="color:grey">nshawn@shareride.com</h>
+                    <h style="color:grey">lnnderitu@shareride.com</h>
+                    <br/><br/>
+
+                    <br/><br/><br/><br/><br/>
+                    <p style="float:right"><font size="-1">2019 Shareride.Inc</font></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                    <center><p><font size="-1">Privacy  Terms  Legal  Site Map  Site Feedback</font></p></center>
+                </div>
+                <div class="col-sm">
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
