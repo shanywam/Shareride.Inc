f2bac99acdd80109f916b6d44b271933e2f02872
shannelle <shannellenjeru@gmail.com>
2019-04-05 13:16:46 +0300

    initial commit

---
+++ driver/drive_page.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href ="./css/bootstrap.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src ="./js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Shareride.Inc</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="main-header-nav" class="collapse navbar-collapse">
+        <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-car"></i> Drive <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-user"></i> Ride <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Service</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Others</a>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div id="ra_header_container_5ca64c7c3a2e8" class="modules-container ra_header_container_5ca64c7c3a2e8  vc_custom_1528890373974"></div>
+    <script type="text/javascript">(function($) {$("head").append("<style>.rella-row-shadowbox-5ca64c7c34b20{-webkit-box-shadow:;-moz-box-shadow:;box-shadow:;}</style>");})(jQuery);</script>
+
+
+
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../image/driver.jpg" class="d-block w-100" alt="..." style="height:500px;color:rgba(255,255,255,1);">
+                </div>
+
+            </div>
+        </div>
+        </div>
+        <div class="col">
+            <h1><span style="color: #00b6bd;">Drive for Shareride.Inc</span></h1>
+            <h2>We&#8217;ll treat you like a professional</h2>
+            <p>The difference between Shareride.Inc and other ride-sharing apps is:</p>
+            <ul>
+                <li><span style="font-weight: 400;">We care about the safety of our drivers.</span></li>
+                <li>We care about our drivers earnings .</li>
+                <li>We care about providing the best tools for our drivers .</li>
+            </ul>
+            <p>Driving for Shareride.Inc is different because we are locally-based in Nairobi. We relate to our drivers in a way other companies don&#8217;t .</p>
+
+        </div>
+</div>
+<style>.ra_button_5ca64f42b1839 i{color:#404041;}.ra_button_5ca64f42b1839{color:#00b6bc;border-color:#00b6bc !important;}.ra_button_5ca64f42b1839:focus,.ra_button_5ca64f42b1839:hover{background-color:#00b6bc;border-color:#00b6bc!important;}</style><a href="http://app.mondoride.com/driver/auth/register" title="Register with Mondo Ride" class="btn btn-default circle btn-md border-thick text-uppercase ra_button_5ca64f42b1839 icon-left" >
+				<span>
+	<i class="fa fa-car"></i>REGISTER TO DRIVE	</span>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href ="./css/bootstrap.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src ="./js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Shareride.Inc</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="main-header-nav" class="collapse navbar-collapse">
+        <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-car"></i> Drive <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i style ="color:blue ;"class="fa fa-user"></i> Ride <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Service</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Others</a>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div id="ra_header_container_5ca64c7c3a2e8" class="modules-container ra_header_container_5ca64c7c3a2e8  vc_custom_1528890373974"></div>
+    <script type="text/javascript">(function($) {$("head").append("<style>.rella-row-shadowbox-5ca64c7c34b20{-webkit-box-shadow:;-moz-box-shadow:;box-shadow:;}</style>");})(jQuery);</script>
+
+
+
+    <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="./image/cab.jpg" class="d-block w-100" alt="..." style="height:500px;color:rgba(255,255,255,1);">
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
