974d0921c8dd2390a884c0814cfd7c1aab5f27f3
shannelle <shannellenjeru@gmail.com>
2019-05-03 19:23:11 +0300

    creation of the registration page

--- auth/register.php
+++ auth/register.php
@@ -1,138 +1,175 @@
 <?php
-include_once '../backend/auth.php';
+session_start();
 
-$user = new User(); // initialize user
+include('../backend/auth.php');
 
-//var_dump(isset($_REQUEST['submit']));
-if (isset($_REQUEST['submit'])) {
-    extract($_REQUEST);
+$reg_user = new Auth();
 
-    $confirm_password = '$password';
-    $register = $user->reg_user($firstname, $lastname, $email, $password, $confirm_password);
+if ($reg_user->is_logged_in()) {
+    if ($_SESSION['user_type'] == 1) {
+        $reg_user->redirect('../driver/drive.php');
+    } else {
+        $reg_user->redirect('../client/ride.php');
+    }
+}
 
-    if ($register) {
-        // redirect to login page
-        header("location: login.php");
-    } //else
-        //{
-        //echo "Something went wrong. Please try again later.";
-        //}
+
+$first_name = $last_name = $email = $phone =$password = $confirm_password = $form_error = "";
+$first_name_err = $last_name_err = $email_err = $phone_err = $password_err = $confirm_password_err = $form_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // validate inputs if empty
+    if (
+        empty(trim($_POST["first_name"]))
+        && empty(trim($_POST["last_name"]))
+        && empty(trim($_POST["email"]))
+        && empty(trim($_POST["phone"]))
+        && empty(trim($_POST["password"]))
+        && empty(trim($_POST["confirm_password"]))
+    ) {
+        $form_error = "Please fill the form before submitting.";
+    }
+
+    // Validate Password
+    if (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Password must have at least 6 characters.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // validate confirm password
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";
+    }elseif (strlen(trim($_POST["confirm_password"])) < 6) {
+        $password_err = "Confirm Password must have at least 6 characters.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+
+        if (empty($password_err) && ($password != $confirm_password)) {
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+
+    // If no errors submit form
+    if (
+        empty($first_name_err) &&
+        empty($last_name_err) &&
+        empty($email_err) &&
+        empty($phone_err) &&
+        empty($password_err) &&
+        empty($confirm_password_err)
+    ) {
+        $email = $_POST["email"];
+
+        $stmt = $reg_user->runQuery("SELECT * FROM users WHERE email = '$email'");
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_store_result($stmt);
+
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+
+            $email_err = "This user email provided already exists.";
+
+        } else {
+
+            $first_name = trim($_POST["first_name"]);
+            $last_name = trim($_POST["last_name"]);
+            $email = trim($_POST["email"]);
+            $phone = trim($_POST["phone"]);
+            $pass = password_hash(trim($_POST["password"]), PASSWORD_DEFAULT);
+
+            if ($reg_user->register($first_name, $last_name, $email, $phone, $pass, 2)){
+                header("location: login.php");
+            }else{
+                $form_err = "Please check your details are correct";
+            }
+
+
+
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href ="../css/bootstrap.min.css"/>
-    <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src ="../js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
 
     <link rel="stylesheet" href="../style/main.css">
-
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Shareride.Inc</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div id="main-header-nav" class="collapse navbar-collapse">
-        <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
-            <li class="nav-item active">
-                <a class="nav-link" href="../index.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class=" nav-item active">
-                <a class="nav-link" href="../auth/register.php"><i style ="color:darkgrey;"class="fa fa-"></i> Register <span class="sr-only">(current)</span> </a>
-            </li>
-            <li class=" nav-item active">
-                <a class="nav-link" href="../auth/login.php"><i style ="color:darkgrey;"class="fa fa-"></i> Login <span class="sr-only">(current)</span> </a>
-            </li>
-        </ul>
-
+<div class="topnav">
+    <div class="container">
+        <a href="../index.php">Home</a>
+        <a href="login.php">Login</a>
+        <a class="active" href="register.php">Register</a>
     </div>
-
-    <div id="ra_header_container_5ca64c7c3a2e8" class="modules-container ra_header_container_5ca64c7c3a2e8  vc_custom_1528890373974"></div>
-    <script type="text/javascript">(function($) {$("head").append("<style>.rella-row-shadowbox-5ca64c7c34b20{-webkit-box-shadow:;-moz-box-shadow:;box-shadow:;}</style>");})(jQuery);</script>
-</nav>
-
+</div>
 
 
 <div class="container">
     <div class="wrapper">
-        <h2 class="text-center">welcome to Shareride.Inc</h2>
-        <p class="text-center">Please fill this form to create an account.</p>
-        <p class="text-center help-block" style="color: red;"></p>
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <h2 class="text-center">shareride</h2>
 
-        <style>
-            #container{width:400px; margin: 0 auto;}
-        </style>
-        <script type="text/javascript" language="javascript">
-            function submitreg()
-            {
-                var form = document.reg;
-                if(form.fname.value == ""){
-                    alert( "Enter name." );
-                    return false;
-                }
-                else if(form.lname.value == ""){
-                    alert( "Enter username." );
-                    return false;
-                }
-                else if(form.email.value == ""){
-                    alert( "Enter email." );
-                    return false;
-                }
-                else if(form.pass.value == ""){
-                    alert( "Enter password." );
-                    return false;
-                }
-                else if(form.confirm_pass.value == ""){
-                    alert( "Enter password." );
-                    return false;
-                }
-            }
-        </script>
-        <div id="container">
-            <h3 class="text-center">Register Here</h3>
-            <form action="" method="post" name="reg">
-                <table>
-                    <tbody>
-                    <tr>
-                        <th>firstname:</th>
-                        <td><input type="text" name="firstname" required="" /></td>
-                    </tr>
-                    <tr>
-                        <th>lastname:</th>
-                        <td><input type="text" name="lastname" required="" /></td>
-                    </tr>
-                    <tr>
-                        <th>email:</th>
-                        <td><input type="text" name="email" required="" /></td>
-                    </tr>
-                    <tr>
-                        <th>password:</th>
-                        <td><input type="password" name="password" required="" /></td>
-                    </tr>
-                    <tr>
-                        <th>confirm_password:</th>
-                        <td><input type="password" name="confirm_password" required="" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input onclick="return(submitreg());" type="submit" name="submit" value="Register" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><a href="login.php">Already registered! Click Here!</a></td>
-                    </tr>
-                    </tbody>
-                </table>
-            </form></div>
+        <p class="text-center">Please fill this form to create an account.</p>
+
+        <p class="text-center help-block" style="color: red;"><?php echo $form_error ?></p>
+
+        <form action="register.php" method="post">
+
+            <div class="<?php echo (!empty($first_name_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>First Name: </label>-->
+                <input type="text" name="first_name" class="input" placeholder="First Name"
+                       value="<?php echo $first_name; ?>">
+                <span class="help-block text-center"><?php echo $first_name_err; ?></span>
+            </div>
+
+            <div class="<?php echo (!empty($last_name_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>Last Name: </label>-->
+                <input type="text" name="last_name" class="input" placeholder="Last Name"
+                       value="<?php echo $last_name; ?>">
+                <span class="help-block text-center"><?php echo $last_name_err; ?></span>
+            </div>
+
+            <div class="<?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>Email: </label>-->
+                <input type="email" name="email" class="input" placeholder="example@gmail.com"
+                       value="<?php echo $email; ?>">
+                <span class="help-block text-center"><?php echo $email_err; ?></span>
+            </div>
+
+            <div class="<?php echo (!empty($phone_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>Phone: </label>-->
+                <input type="text" name="phone" class="input" placeholder="phone no"
+                       value="<?php echo $phone; ?>">
+                <span class="help-block text-center"><?php echo $phone_err; ?></span>
+            </div>
+
+            <div class="<?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>Password: </label>-->
+                <input type="password" name="password" class="input" placeholder="password"
+                       value="<?php echo $password; ?>">
+                <span class="help-block text-center"><?php echo $password_err; ?></span>
+            </div>
+
+            <div class="<?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <!--                <label>Confirm Password: </label>-->
+                <input type="password" name="confirm_password" class="input" placeholder="confirm password"
+                       value="<?php echo $confirm_password; ?>">
+                <span class="help-block text-center"><?php echo $confirm_password_err; ?></span>
+            </div>
+
+            <div class="">
+                <input type="submit" class="btn btn-primary input" value="Submit">
+            </div>
+            <p class="text-center">Already have an account? <a href="login.php">LOGIN HERE</a>.</p>
+        </form>
     </div>
 </div>
 </body>
 </html>
+
+
