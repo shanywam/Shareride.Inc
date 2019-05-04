b5a7122e6dbbe8ef138e31c88f4ae5a23eeaa4cd
shannelle <shannellenjeru@gmail.com>
2019-05-05 01:46:25 +0300

    changes made on the nav bar and additional content done

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href ="./css/bootstrap.min.css"/>
+    <link rel="stylesheet" href ="/css/bootstrap.min.css"/>
     <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src ="./js/bootstrap.min.js"></script>
@@ -18,6 +18,7 @@
     </button>
 
     <div id="main-header-nav" class="collapse navbar-collapse">
+
         <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
             <li class="nav-item active">
                 <a class="nav-link" href="index.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
@@ -30,17 +31,7 @@
             </li>
         </ul>
 
-    </div>
 
-    <div id="ra_header_container_5ca64c7c3a2e8" class="modules-container ra_header_container_5ca64c7c3a2e8  vc_custom_1528890373974"></div>
-    <script type="text/javascript">(function($) {$("head").append("<style>.rella-row-shadowbox-5ca64c7c34b20{-webkit-box-shadow:;-moz-box-shadow:;box-shadow:;}</style>");})(jQuery);</script>
-
-
-
-    <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
 </nav>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -52,6 +43,35 @@
 
     </div>
 </div>
+
+<br><br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="/image/cab11.jpg" class="d-block w-100" alt="..." style="height:500px;width:50%;color:rgba(255,255,255,1);border-radius:20px ">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h1><span style="color: #00b6bd;font-family: 'Dosis', sans-serif;">About us </span></h1>
+
+            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">Shareride.Inc is a ride hailing website based in Nairobi, Kenya. We provide safe and reliable transport in the city of Nairobi.</span></p>
+            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">we provide safe and reliable transport in the city of Nairobi.</span></p>
+
+
+
+        </div>
+    </div>
+</div>
+
+<br><br><br>
+
 <div>
 
     <div style="background-color:rgba(211,211,211,0.5);">
