f56def0554b69f04fac7d0845b1687c7c5035ed6
shannelle <shannellenjeru@gmail.com>
2019-05-05 01:44:57 +0300

    changes made on the nav bar

--- auth/register.php
+++ auth/register.php
@@ -96,18 +96,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
+
+    <link rel="stylesheet" href ="../css/bootstrap.min.css"/>
+    <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src ="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Dosis|PT+Sans+Narrow" rel="stylesheet">
+
+
     <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
 
     <link rel="stylesheet" href="../style/main.css">
 </head>
 <body>
-<div class="topnav">
-    <div class="container">
-        <a href="../index.php">Home</a>
-        <a href="login.php">Login</a>
-        <a class="active" href="register.php">Register</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Shareride.Inc</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="main-header-nav" class="collapse navbar-collapse">
+
+        <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
+            <li class="nav-item active">
+                <a class="nav-link" href="../index.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class=" nav-item active">
+                <a class="nav-link" href="../auth/register.php"><i style ="color:darkgrey;font-family: 'Merienda', cursive;"class="fa fa-"></i> Register <span class="sr-only">(current)</span> </a>
+            </li>
+            <li class=" nav-item active">
+                <a class="nav-link" href="../auth/login.php"><i style ="color:darkgrey;font-family: 'Merienda', cursive;"class="fa fa-"></i> Login <span class="sr-only">(current)</span> </a>
+            </li>
+        </ul>
+
+
     </div>
-</div>
+</nav>
 
 
 <div class="container">
