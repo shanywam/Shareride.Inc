e99e327c5921e2f1de147f652f4d16c0e8e7067b
shannelle <shannellenjeru@gmail.com>
2019-05-05 01:36:43 +0300

    creation of the database configaration file

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,5 +1,6 @@
 <?php
-class DbConfig
+
+class Database
 {
     // database connection variables
     private $host = "127.0.0.1";
@@ -7,22 +8,24 @@ class DbConfig
     private $password = "secret";
     private $database = "shareride";
     private $port = "3306";
+
     public $conn;
 
-    // Database connection function
-    public function connect()
+    // database connection function
+    public function dbConnect()
     {
         $this->conn = null;
 
-        try{
+        try {
 
-            $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->database, $this->port);
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
 
-        } catch(mysqli_sql_exception $exception) {
+        } catch (mysqli_sql_exception $exception) {
 
-            echo "Connection error" . $exception->getMessage();
+            echo "Connection error: " . $exception->getMessage();
         }
 
         return $this->conn;
     }
 }
+
