6a5e7c4b28095010650f7e36bc4388efacd4c394
shannelle <shannellenjeru@gmail.com>
2019-05-07 19:20:23 +0300

    additional content on client reserving a ride

--- client/rideform.php
+++ client/rideform.php
@@ -1,67 +1,90 @@
 <?php
+session_start();
 
+include('../backend/auth.php');
 
-include('../backend/db_config.php');
+$book_ride = new Auth();
 
-$connection = new Database();
+$origin = $destination = $vehicle_capacity = $identification  = $form_error = "";
+$reservation_id = $success_message = '';
+$origin_err = $destination_err = $vehicle_capacity_err = $identification_err = $form_err = "";
 
-$conn = $connection->dbConnect();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if (isset($_POST['rideForm'])) {
+    // validate inputs if empty
+    if (isset($_POST['create_new'])) {
 
+        if (
+            empty(trim($_POST["origin"]))
+            && empty(trim($_POST["destination"]))
+            && empty(trim($_POST["vehicle_capacity"]))
+            && empty(trim($_POST["identification"]))
 
+        //var_dump($_POST);
 
-    $origin = $_POST['origin'];
-
-    $destination = $_POST['destination'];
-
-    $vehicle_capacity = $_POST['vehicle_capacity'];
-
-    $identification = $_POST['identification'];
-
-
-
-    $type = 2;
-
-
-    if (!empty($origin) && !empty($destination) && !empty($vehicle_capacity && !empty($identification)) ) {
-
-
-        $results = $conn->query("SELECT * from reserevations WHERE identification='$identification'");
-
-        $count = $results->num_rows;
-
-        if ($count == 0) {
-
-            $query = "INSERT into reservations(  origin, destination, vehicle_capacity, identification, client_id,  driver_id) VALUES(?, ?, ?, ?, ?, ?)";
-
-            $statement = $conn->prepare($query);
-
-            $statement->bind_param('ssiiii',  $origin, $destination, $capacity_of_people, $identification,$client_id, $driver_id);
-
-            if ($statement->execute()) {
-
-                echo "Success";
-
-            } else {
-
-                die('There was a problem');
-
-            }
-
-        } else {
-
-            echo "Sorry the Identification Exist";
+        ) {
+            $form_error = "Please fill the form before submitting.";
         }
 
 
+        // If no errors submit form
+        if (
+            empty($origin_err) &&
+            empty($destination_err) &&
+            empty($vehicle_capacity_err) &&
+            empty($identification_err) ) {
+            $identification = $_POST["identification"];
+
+            $stmt = $book_ride->runQuery("SELECT * FROM reservations WHERE identification = '$identification'");
+
+            mysqli_stmt_execute($stmt);
+
+            mysqli_stmt_store_result($stmt);
+
+            if (mysqli_stmt_num_rows($stmt) == 1) {
+
+                $identification_err = "This identification number provided already exists.";
+
+            } else {
+
+                $origin = trim($_POST["origin"]);
+                $destination = trim($_POST["destination"]);
+                $vehicle_capacity = trim($_POST["vehicle_capacity"]);
+                $identification = trim($_POST["identification"]);
+
+                if ($book_ride->ride ($origin, $destination, $vehicle_capacity, $identification, 2)) {
+                    header("location: ride.php");
+                } else {
+                    $form_err = "Please check if your details are correct";
+                }
+
+            }
+        }
+    }elseif (isset($_POST['edit_action'])) {
+
+        $reservation_id = $_POST["book_id"];
+
+        $details = $book_ride->getReservationDetails($reservation_id);
+
+        $origin = $details['origin'];
+        $destination = $details['destination'];
+        $vehicle_capacity = $details['vehicle_capacity'];
+        $identification = $details['identification'];
+
+    }elseif (isset($_POST['create_edit'])) {
+        
+        if($book_ride->editReservation()){
+            $book_ride->redirect("list.php");
+            // $success_message = "Edited Successfully";
+        }else{
+            $form_error = "Edit process unsuccessful";
+        }
+
     }
 
+
 }
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -83,12 +106,20 @@ if (isset($_POST['rideForm'])) {
 
     <div id="main-header-nav" class="collapse navbar-collapse">
         <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
-            <li class="nav-item active">
-                <a class="nav-link" href="../client/ride.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class=" nav-item active">
-                <a class="nav-link" href="../auth/logout.php"><i style ="color:darkgrey; font-family: 'Merienda', cursive;"class="fa fa-"></i> Logout <span class="sr-only">(current)</span> </a>
-            </li>
+            <ul id="menu-main-nav" class="nav navbar-nav main-nav underlined weight-light">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../client/ride.php"><i style ="color:darkgrey ;"class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../client/rideform.php"><i style ="color:darkgrey ;"class="fa fa-car"></i> RequestRide <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../client/list.php"><i style ="color:darkgrey ;"class="fa fa-list"></i>List <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../auth/logout.php"><i style ="color:darkgrey ;"class="fa fa-sign-out-alt"></i> Logout <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
         </ul>
     </div>
 
@@ -105,33 +136,41 @@ if (isset($_POST['rideForm'])) {
 
         <p class="text-center help-block" style="color: red;"></p>
 
-        <?php
-        //echo $form_error
-        ?>
+        <?php echo $form_error; ?>
+        <?php echo $success_message; ?>
 
         <form style="width: 400px;background: #fcfcfc;margin: 70px auto;" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
             <div class="form-group">
-                <label for="formGroupExampleInput2">Origin</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" name="origin" placeholder="Pick up point" >
+                <label for="formGroupExampleInput2">Origin Location</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" name="origin" placeholder="Pick up point"  value="<?php echo $origin; ?>">
             </div>
             <div class="form-group">
-                <label for="formGroupExampleInput2">Destination</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" name="destination" placeholder="Where to" >
+                <label for="formGroupExampleInput2">Destination Location</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" name="destination" placeholder="Where to" value="<?php echo $destination; ?>">
             </div>
 
             <div class="form-group">
-                <label for="formGroupExampleInput2">capacity_of_people</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2"  name="capacity_of_people" placeholder="No of people" >
+                <label for="formGroupExampleInput2">vehicle_capacity</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2"  name="vehicle_capacity" placeholder="No of people" value="<?php echo $vehicle_capacity; ?>">
             </div>
             <div class="form-group">
-                <label for="formGroupExampleInput2">Identification</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Your ID No" name="identification" >
+                <label for="formGroupExampleInput2">National Id</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Your ID No" name="identification" value="<?php echo $identification; ?>">
             </div>
+
+
+            <?php if ($reservation_id) { ?>
+               <input type="hidden" name="reservation_id" value="<?php echo $reservation_id; ?>" />
+
+               <input type="hidden" name="create_edit" value="create_edit">
+            <?php } else { ?>
+                <input type="hidden" name="create_new" value="create_new">
+            <?php } ?>
+
             <button type="submit" name="driverForm" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </div>
-cd
 </body>
 </html>
