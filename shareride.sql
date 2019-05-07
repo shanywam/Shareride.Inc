714c11341dd2ef85b2f1205a2f035f3d41f14afd
shannelle <shannellenjeru@gmail.com>
2019-05-07 21:16:23 +0300

    sql dump folders

--- shareride.sql
+++ shareride.sql
@@ -16,63 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `available_rides`
+-- Table structure for table `reservations`
 --
 
-DROP TABLE IF EXISTS `available_rides`;
+DROP TABLE IF EXISTS `reservations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `available_rides` (
+CREATE TABLE `reservations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ride_id` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
+  `origin` varchar(200) NOT NULL,
+  `destination` varchar(200) NOT NULL,
+  `vehicle_capacity` int(11) NOT NULL,
+  `identification` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL DEFAULT '0',
+  `driver_id` int(11) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_available_rides_rides1_idx` (`ride_id`),
-  CONSTRAINT `fk_available_rides_rides1` FOREIGN KEY (`ride_id`) REFERENCES `rides` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `fk_rides_users1_idx` (`driver_id`),
+  CONSTRAINT `fk_rides_users1` FOREIGN KEY (`driver_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='\n';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `available_rides`
+-- Dumping data for table `reservations`
 --
 
-LOCK TABLES `available_rides` WRITE;
-/*!40000 ALTER TABLE `available_rides` DISABLE KEYS */;
-/*!40000 ALTER TABLE `available_rides` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `driver`
---
-
-DROP TABLE IF EXISTS `driver`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `driver` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `firstname` varchar(200) NOT NULL,
-  `lastname` varchar(200) NOT NULL,
-  `email` varchar(200) DEFAULT NULL,
-  `identification_no` varchar(200) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_driver_users1_idx` (`user_id`),
-  CONSTRAINT `fk_driver_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `driver`
---
-
-LOCK TABLES `driver` WRITE;
-/*!40000 ALTER TABLE `driver` DISABLE KEYS */;
-/*!40000 ALTER TABLE `driver` ENABLE KEYS */;
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (1,'Utawala','Nakuru',1,123223,0,NULL,NULL,NULL,NULL),(3,'Kitui','Kinoo',3,987654,0,NULL,NULL,NULL,NULL),(4,'Nakuru','Kisumu',10,987654321,0,NULL,NULL,NULL,NULL),(5,'Embakasi','JKIA',5,36265876,0,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -84,16 +58,16 @@ DROP TABLE IF EXISTS `rides`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rides` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `origin` varchar(200) NOT NULL,
-  `departure` varchar(200) NOT NULL,
-  `capacity_of_vehicle` varchar(45) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `reservation_id` int(11) NOT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_rides_users2_idx` (`user_id`),
+  KEY `fk_rides_reservations1_idx` (`reservation_id`),
+  CONSTRAINT `fk_rides_reservations1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_rides_users2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,65 +76,7 @@ CREATE TABLE `rides` (
 
 LOCK TABLES `rides` WRITE;
 /*!40000 ALTER TABLE `rides` DISABLE KEYS */;
-INSERT INTO `rides` VALUES (1,'shannelle',719582000,'utawala','westlands','2',NULL,NULL,NULL),(5,'sean',719582000,'cbd','upperhill','5',NULL,NULL,NULL),(6,'one',712345678,'cbd','upperhill','56',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `rides` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_booking_ride_details`
---
-
-DROP TABLE IF EXISTS `user_booking_ride_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_booking_ride_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `available_ride_id` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_user_booking_ride_details_user1_idx` (`user_id`),
-  KEY `fk_user_booking_ride_details_available_rides1_idx` (`available_ride_id`),
-  CONSTRAINT `fk_user_booking_ride_details_available_rides1` FOREIGN KEY (`available_ride_id`) REFERENCES `available_rides` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_booking_ride_details_user1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_booking_ride_details`
---
-
-LOCK TABLES `user_booking_ride_details` WRITE;
-/*!40000 ALTER TABLE `user_booking_ride_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_booking_ride_details` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_ride_assignment`
---
-
-DROP TABLE IF EXISTS `user_ride_assignment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_ride_assignment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ride_assignment id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_ride_assignment`
---
-
-LOCK TABLES `user_ride_assignment` WRITE;
-/*!40000 ALTER TABLE `user_ride_assignment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_ride_assignment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -198,19 +114,19 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(200) NOT NULL,
-  `lastname` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `confirm_password` varchar(200) NOT NULL,
-  `user_type_id` int(11) DEFAULT NULL,
+  `user_type` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_user_user_type1_idx` (`user_type_id`),
-  CONSTRAINT `fk_user_user_type1` FOREIGN KEY (`user_type_id`) REFERENCES `user_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  KEY `fk_user_user_type1_idx` (`user_type`),
+  CONSTRAINT `fk_user_user_type1` FOREIGN KEY (`user_type`) REFERENCES `user_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -219,7 +135,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'\"mee\"','\"mee\"','\"mee@gmail.com\"','\"e10adc3949ba59abbe56e057f20f883e\"','\"123456\"',NULL,NULL,NULL,NULL),(2,'\"mee\"','\"mee\"','\"mee@gmail.com\"','\"e10adc3949ba59abbe56e057f20f883e\"','\"123456\"',NULL,NULL,NULL,NULL),(3,'\"sean\"','\"sean\"','\"sean@gmail.com\"','\"e10adc3949ba59abbe56e057f20f883e\"','\"e10adc3949ba59abbe56e057f20f883e\"',NULL,NULL,NULL,NULL),(4,'shanny','baby','shanny@gmail.com','ad6136dbb947be526498c75171bb603d','ad6136dbb947be526498c75171bb603d',NULL,NULL,NULL,NULL),(5,'test','test','test@gmail.com','098f6bcd4621d373cade4e832627b4f6','098f6bcd4621d373cade4e832627b4f6',NULL,NULL,NULL,NULL),(6,'kiboi','em','em@gmail.com','c82dd414deb57467afbe564f9c57031d','243e61e9410a9f577d2d662c67025ee9',NULL,NULL,NULL,NULL),(7,'susan','muraya','susanm@gmail.com','c82dd414deb57467afbe564f9c57031d','243e61e9410a9f577d2d662c67025ee9',NULL,NULL,NULL,NULL),(8,'val','muthoni','val@gmail.com','c82dd414deb57467afbe564f9c57031d','243e61e9410a9f577d2d662c67025ee9',NULL,NULL,NULL,NULL),(9,'bradly','juma','bradly@gmail.com','c82dd414deb57467afbe564f9c57031d','243e61e9410a9f577d2d662c67025ee9',NULL,NULL,NULL,NULL),(10,'luis','bb','luis@gmail.com','c82dd414deb57467afbe564f9c57031d','243e61e9410a9f577d2d662c67025ee9',NULL,NULL,NULL,NULL),(11,'elle','wam','elle@gmail.com','1788613c5e5fb7a405c2b96b493881d2','243e61e9410a9f577d2d662c67025ee9',2,NULL,NULL,NULL),(12,'shanny','wamaitha','shannywam@gmail.com','1788613c5e5fb7a405c2b96b493881d2','243e61e9410a9f577d2d662c67025ee9',2,NULL,NULL,NULL);
+INSERT INTO `users` VALUES (1,'shanel','wam','shanelwan1_@gmail.com','0712345678','$2y$10$OVI2WFs9Dejd.4.fvCLEe.KIoB2VarW6vYVXh72F5vMFbPglAC7.C',1,NULL,NULL,NULL),(2,'lawie','ndwiga','lnndwiga@yahoo.com','0720389444','$2y$10$DEwjGZ9juScx5XrsY4.W5OC6CZ//UoqknxT8Mj3h20GHF0p.HgI2y',1,NULL,NULL,NULL),(3,'shan','wam','shanwam@gmail.com','0789866543','$2y$10$BT6NAUJoQbLrQYij/0cgE.2zuuewtLnQ5JCRX389xljNKEMGBn7AO',1,NULL,NULL,NULL),(4,'shany','wam','shanywam@gmail.com','0987654321','$2y$10$oCCR/3zXuwZnmlU7Ey84yO2nO72gvc/aKikIydtXS3j.tWUyl/6Eu',2,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -232,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-23 19:52:29
+-- Dump completed on 2019-05-07 18:06:17
