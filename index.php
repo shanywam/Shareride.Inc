c6302aa4b434ccdfb203b33e4fb63dc67fd0c272
shannelle <shannellenjeru@gmail.com>
2019-05-07 19:25:47 +0300

    additional content on this file

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 <html>
 <head>
     <link rel="stylesheet" href ="/css/bootstrap.min.css"/>
-    <script src="../https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="../https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src ="./js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src ="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <title>Shareride.Inc &#8211; Kenya&#039;s Taxi App &#8211; Based in Nairobi</title>
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Dosis|PT+Sans+Narrow" rel="stylesheet">
 
@@ -60,9 +58,14 @@
         </div>
         <div class="col">
             <h1><span style="color: #00b6bd;font-family: 'Dosis', sans-serif;">About us </span></h1>
-
             <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">Shareride.Inc is a ride hailing website based in Nairobi, Kenya. We provide safe and reliable transport in the city of Nairobi.</span></p>
-            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">we provide safe and reliable transport in the city of Nairobi.</span></p>
+            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">Consequently, we set out to deliver unbeatable quality vehicles to suit unique customer needs.</span></p>
+            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">Today, we guarantee quality service, maximum client satisfaction with unparalleled offer for best value for money to our clients!</span></p>
+
+            <h1><span style="color: #00b6bd;font-family: 'Dosis', sans-serif;">Client Saftety</span></h1>
+            <p><span style="font-weight: 400;font-family: 'Dosis', sans-serif;">From the moment you request a ride to the moment you arrive, the Shareride.Inc experience has been designed from the ground up with your safety in mind.</span></p>
+
+
 
 
 
@@ -79,9 +82,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm">
-                    <b><h>About</h></b><br/><br/>
-                    <h style="color:grey;font-family: 'Dancing Script', cursive;">Shareride.Inc is a ride hailing website based in Nairobi, Kenya.</h><br/>
-                    <h style="color:grey;font-family: 'Dancing Script', cursive;">we provide safe and reliable transport in the city of Nairobi.</h><br/>
+                    <b><h>Emails</h></b><br/><br/>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">pnjeru_@shareride.com</h>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">nshawn@shareride.com</h>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">lnnderitu@shareride.com</h>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">agnes_@shareride.com</h>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">sheila..@shareride.com</h>
+                    <h style="color:grey;font-family: 'Dancing Script', cursive;">pmwaura_*@shareride.com</h>
                     <br/><br/>
 
                 </div>
@@ -99,17 +106,7 @@
                     <h style="color:grey;font-family: 'Dancing Script', cursive;">+254 71 442 8358 </h><br/>
                     <br/><br/>
                 </div>
-                <div class="col-sm">
-                    <b><h>Emails</h></b><br/><br/>
-                    <h style="color:grey;font-family: 'Dancing Script', cursive;">pnjeru_@shareride.com</h>
-                    <h style="color:grey;font-family: 'Dancing Script', cursive;">nshawn@shareride.com</h>
-                    <h style="color:grey;font-family: 'Dancing Script', cursive;">lnnderitu@shareride.com</h>
-                    <br/><br/>
 
-                    <br/><br/><br/><br/><br/>
-                    <p style="float:right;font-family: 'Dancing Script', cursive;"><font size="-1">2019 Shareride.Inc</font></p>
-
-                </div>
             </div>
         </div>
         <div class="container">
@@ -118,6 +115,8 @@
                 </div>
                 <div class="col-sm">
                     <center><p><font size="-1"><span style="font-family: 'Dancing Script', cursive;">Privacy  Terms  Legal  Site Map  Site Feedback</span></font></p></center>
+                    <center><p><font size="-1"><span style="font-family: 'Dancing Script', cursive;">2019 Shareride.Inc</span></font></p></center>
+
                 </div>
                 <div class="col-sm">
                 </div>
@@ -125,5 +124,11 @@
         </div>
     </div>
 
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 </body>
 </html>
